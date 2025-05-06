4622f89bf0ad8cca6ea3732d38dd72198cdb732d
고윤아 <0205ria@gmail.com>
2025-05-06 18:19:48 +0900

    ✨Add life count ui
    * Added Heart ui for life count

--- 2DVillage/Assets/Scripts/Entity/StatHandler.cs
+++ 2DVillage/Assets/Scripts/Entity/StatHandler.cs
@@ -1,25 +1,16 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class StatHandler : MonoBehaviour
 {
-    [Range (1, 100)][SerializeField] private int health = 10;
-    public int Health
-    {
+    [Range(1,100)] [SerializeField] private int health = 10;
+    public int Health{
         get => health;
-        set => health = Mathf.Clamp(value, 0, 100);
-    }
+        set => health = Mathf.Clamp(value,0,100);
+    } 
     
-    [Range (1f, 20f)][SerializeField] private float speed = 3f;
-    public float Speed
-    {
+    [Range(1f, 20f)] [SerializeField] private float speed = 3;
+    public float Speed{
         get => speed;
-        set => speed = Mathf.Clamp(value, 0f, 20f);
+        set => speed = Mathf.Clamp(value,0,20);
     }
-    
-    
-    
-    
-    
 }
--- 2DVillage/Assets/Scripts/UI/UIManager.cs
+++ 2DVillage/Assets/Scripts/UI/UIManager.cs
@@ -1,11 +1,33 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour
 {
     public static UIManager Instance { get; private set; }
+    public Sprite heartFull, heartHalf, heartEmpty;
+    public Image[] heartImages;
 
     private void Awake()
     {
         if (Instance == null) Instance = this;
+    }
+    
+    public void UpdateHeartUI(int currentLife, int maxLife)
+    {
+        int heartCount = heartImages.Length;
+
+        for (int i = 0; i < heartCount; i++)
+        {
+            int value = currentLife - (i * 2);
+        
+            if (value >= 2)
+                heartImages[i].sprite = heartFull;
+            else if (value == 1)
+                heartImages[i].sprite = heartHalf;
+            else
+                heartImages[i].sprite = heartEmpty;
+
+            heartImages[i].enabled = (i * 2 < maxLife);
+        }
     }
 }
