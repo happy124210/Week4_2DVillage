4d1333b6ee887be535fde0a461c9d8aabd66c3b2
고윤아 <0205ria@gmail.com>
2025-05-07 13:17:01 +0900

    🐛Fix Player rotation bug
    * Player now rotates correctly toward mouse pointer

--- 2DVillage/Assets/Scripts/Entity/BaseController.cs
+++ 2DVillage/Assets/Scripts/Entity/BaseController.cs
@@ -45,6 +45,7 @@ namespace Entity
         protected virtual void Update()
         {
             HandleAction();
+            Rotate(lookDirection);
         }
 
 
@@ -75,6 +76,15 @@ namespace Entity
 
             _rigidbody.velocity = direction;
             animationHandler.Move(direction);
+        }
+        
+        
+        private void Rotate(Vector2 direction)
+        {
+            float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            bool isLeft = Mathf.Abs(rotZ) > 90f;
+
+            characterRenderer.flipX = isLeft;
         }
 
 
